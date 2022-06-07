cb0f5bfd9f93da0e7b23148f045bce1b517b7935
Kuba Karpierz <mr.karpierz@gmail.com>
2022-06-07 14:13:00 -0400

    applied pixel changes directly on image data

--- www/index.js
+++ www/index.js
@@ -10,43 +10,29 @@ const ctx = canvas.getContext('2d');
 ctx.canvas.width = display.width();
 ctx.canvas.height = display.height();
 
+
+var data = ctx.createImageData(ctx.canvas.width, ctx.canvas.height);
+var buf = new Uint32Array(data.data.buffer);
+
+const drawPixel = (x, y, r, g, b) => {
+    const index = x * width + y;
+    buf[index] = r + (g << 8) + (b << 16) + (255 << 24);
+}
+
 const start = Date.now();
-
-function clamp(num, min, max) {
-    return num <= min
-        ? min
-        : num >= max
-            ? max
-            : num
-}
-
-const toHex = (val) => {
-    let s = Math.round(clamp(val, 0, 256)).toString(16).toUpperCase();
-    while (s.length < 2) {
-        s = "0" + s;
-    }
-    return s;
-}
-
-const color = (r, g, b) => {
-    return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
-}
 
 const drawPixels = () => {
     console.log("rendering");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
     display.tick();
     const pixelsPtr = display.pixels();
     const pixels = new Uint8Array(memory.buffer, pixelsPtr, 3 * width * height);
     for (let x = 0; x < display.width(); x++) {
         for (let y = 0; y < display.height(); y++) {
             const index = 3 * (x * width + y);
-            ctx.fillStyle = color(pixels[index], pixels[index + 1], pixels[index + 2]);
-            ctx.fillRect(x, y, 1, 1);
+            drawPixel(x, y, pixels[index], pixels[index + 1], pixels[index + 2]);
         }
     }
-    ctx.stroke();
+    ctx.putImageData(data, 0, 0);
 };
 
 const renderLoop = () => {
