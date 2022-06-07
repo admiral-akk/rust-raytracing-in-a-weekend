063bd44c145a19bcc97000a66a623e9446ff0831
Kuba Karpierz <mr.karpierz@gmail.com>
2022-06-07 12:47:56 -0400

    Added scrolling

--- www/index.js
+++ www/index.js
@@ -3,12 +3,14 @@ import * as wasm from "rust-ray";
 //wasm.greet();
 
 
-const width = 100;
-const height = 100;
+const width = 1000;
+const height = 1000;
 const canvas = document.getElementById("raytracing-canvas");
 const ctx = canvas.getContext('2d');
 ctx.canvas.width = width;
 ctx.canvas.height = height;
+
+const start = Date.now();
 
 function clamp(num, min, max) {
     return num <= min
@@ -34,12 +36,13 @@ const color = (x, y) => {
 
 const drawPixels = () => {
     ctx.beginPath();
+    const time = Date.now() - start;
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
             ctx.fillStyle = color(x, y);
             ctx.fillRect(
-                x,
-                y,
+                (x + time) % width,
+                (y + time) % height,
                 1,
                 1
             );
