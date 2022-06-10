a7aa1cf3ad86526f3e93dfdf8d531cf42da125ac
Kuba Karpierz <mr.karpierz@gmail.com>
2022-06-10 11:09:32 -0400

    Added camera specific dir vectors

--- src/scene/camera.rs
+++ src/scene/camera.rs
@@ -1,11 +1,14 @@
 use crate::{
-    math::vector::{self, DOWN, RIGHT},
+    math::vector::{self, DOWN, RIGHT, UP},
     Color, Ray, Vec3, World,
 };
 
 pub struct Camera {
     pos: Vec3,
     dir: Vec3,
+    up: Vec3,
+    right: Vec3,
+    forward: Vec3,
     focal_length: f32,
     viewport_height: f32,
     viewport_width: f32,
@@ -15,21 +18,27 @@ impl Camera {
     pub fn new(aspect_ratio: f32, vertical_fov_degrees: f32) -> Camera {
         let rad_angle = std::f32::consts::PI * vertical_fov_degrees / 180.0;
         let h = 2.0 * f32::tan(rad_angle / 2.0);
+        let w = h * aspect_ratio;
+        let focal_length = 1.0;
+
         Camera {
             pos: vector::ZERO,
             dir: vector::FORWARD,
+            up: h * vector::DOWN,
+            right: w * vector::RIGHT,
+            forward: focal_length * vector::FORWARD,
             focal_length: 1.0,
             viewport_height: h,
-            viewport_width: h * aspect_ratio,
+            viewport_width: w,
         }
     }
 
     pub fn color(&self, x: f32, y: f32, world: &World) -> Color {
         let view_x = x - 0.5;
         let view_y = y - 0.5;
-        let focal_center = self.focal_length * self.dir;
-        let x_delta = self.viewport_width * view_x * RIGHT;
-        let y_delta = self.viewport_height * view_y * DOWN;
+        let focal_center = self.forward;
+        let x_delta = view_x * self.right;
+        let y_delta = view_y * self.up;
         let mut ray = Ray {
             pos: self.pos,
             dir: (focal_center + y_delta + x_delta).normalized(),
