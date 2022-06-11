9b3b03b644b8f7a3fb05f79eb51dc2590484842b
Kuba Karpierz <mr.karpierz@gmail.com>
2022-06-11 17:38:28 -0400

    seperated random scene initialization

--- src/scene/world.rs
+++ src/scene/world.rs
@@ -23,7 +23,29 @@ impl World {
     pub fn push(&mut self, object: Object) {
         self.objects.push(object);
     }
+}
 
+impl<'a> World {
+    pub fn hit(&'a self, ray: &Ray) -> HitRecord<'a> {
+        let mut ret: HitRecord = hit_record::DEFAULT;
+        let mut temp: HitRecord = hit_record::DEFAULT;
+        for hittable in &self.objects {
+            hittable.hit(ray, &mut temp);
+            if !temp.hit() {
+                continue;
+            }
+            if temp.t < ret.t {
+                ret.t = temp.t;
+                ret.normal = temp.normal;
+                ret.point = temp.point;
+                ret.object = Some(hittable);
+            }
+        }
+        return ret;
+    }
+}
+
+impl World {
     pub fn random_scene(&mut self, range: i32) {
         let mut rng = thread_rng();
 
@@ -104,25 +126,5 @@ impl World {
                 0.0,
             )),
         ));
-    }
-}
-
-impl<'a> World {
-    pub fn hit(&'a self, ray: &Ray) -> HitRecord<'a> {
-        let mut ret: HitRecord = hit_record::DEFAULT;
-        let mut temp: HitRecord = hit_record::DEFAULT;
-        for hittable in &self.objects {
-            hittable.hit(ray, &mut temp);
-            if !temp.hit() {
-                continue;
-            }
-            if temp.t < ret.t {
-                ret.t = temp.t;
-                ret.normal = temp.normal;
-                ret.point = temp.point;
-                ret.object = Some(hittable);
-            }
-        }
-        return ret;
     }
 }
--- tests/test.rs
+++ tests/test.rs
@@ -16,7 +16,7 @@ mod tests {
         world.random_scene(0);
 
         let ray = Ray::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 1.0));
-        for _i in 0..1000000 {
+        for _i in 0..100000000 {
             world.hit(&ray);
         }
     }
@@ -25,9 +25,10 @@ mod tests {
     fn full_world() {
         let mut world = World::new();
         world.random_scene(11);
+        let world = world;
 
         let ray = Ray::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 1.0));
-        for _i in 0..1000000 {
+        for _i in 0..2000000 {
             world.hit(&ray);
         }
     }
