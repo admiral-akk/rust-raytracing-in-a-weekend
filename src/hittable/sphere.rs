3a9bf91c1055c82f5ba84828bc25ad19092688af
Kuba Karpierz <mr.karpierz@gmail.com>
2022-06-22 01:16:41 -0400

    Improved small problem time

--- src/hittable/sphere.rs
+++ src/hittable/sphere.rs
@@ -19,6 +19,7 @@ impl Sphere {
 
 impl Hittable for Sphere {
     // This uses the quadratic equation. a == 1, since the ray.dir is normalized.
+    #[inline(always)]
     fn hit(&self, ray: &Ray) -> f32 {
         let diff = &self.pos - &ray.pos;
         let c = &diff * &diff - self.radius_sq;
@@ -39,7 +40,6 @@ impl Hittable for Sphere {
         return t;
     }
 
-    #[inline]
     fn hit_normal(&self, _ray: &Ray, hit_point: &Vec3) -> Vec3 {
         return (hit_point - &self.pos).normalized();
     }
